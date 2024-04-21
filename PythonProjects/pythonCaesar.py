53ef2da652a41a18d01b864e985f979246d8b586
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-04-21 12:54:27 -0700

    Update pythonCaesar.py with file io

--- PythonProjects/pythonCaesar.py
+++ PythonProjects/pythonCaesar.py
@@ -8,6 +8,20 @@ def caesar_cipher(text, key, encrypt = True):
         translation_table = str.maketrans(shifted_alphabet, alphabet)
     return text.translate(translation_table)
 
+def encrypt_file(input_file, key):
+    with open(input_file, "r") as file:
+        text = file.read()
+    encrypted_text = caesar_cipher(text, key)
+    with open(f"{input_file}_encrypted.txt", 'w') as file:
+        file.write(encrypted_text)
+
+def decrypt_file(input_file, key):
+    with open(input_file, "r") as file:
+        text = file.read()
+    encrypted_text = caesar_cipher(text, key, encrypt=False)
+    with open(f"{input_file}_decrypted.txt", 'w') as file:
+        file.write(encrypted_text)
+
 #example of how to use (let key be 5)
 text = "This is a message to encode!"
 shiftKey = 5
@@ -17,3 +31,16 @@ print(encrypted_text)
 #example of how to decrypt
 decrypted_text = caesar_cipher(encrypted_text, shiftKey, encrypt=False)
 print(decrypted_text)
+
+#example of file encryption (let key be 5)
+filename = 'C:\\Users\\Conner Batson\\Documents\\Python\\PythonProjects\\caesar.txt'
+encrypt_file(filename, shiftKey)
+
+with open(f"{filename}_encrypted.txt", 'r') as file:
+    file.read()
+
+filename2 = f"{filename}_encrypted.txt"
+decrypt_file(filename2, shiftKey)
+
+with open(f"{filename2}_decrypted.txt", 'r') as file:
+    file.read()
