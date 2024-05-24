c2211d65df36b54ed3d42a046bfb14631c793689
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-24 13:58:41 -0700

    Update soup.py to utilize article tags and datetime

--- PythonProjects/scraper/soup.py
+++ PythonProjects/scraper/soup.py
@@ -1,5 +1,6 @@
 import requests, random
 from bs4 import BeautifulSoup
+import datetime
 
 url = "https://pixelford.com/blog"
 random_num = random.randint(1,999999)
@@ -8,9 +9,23 @@ html = response.content
 soup = BeautifulSoup(html, 'html.parser')
 a_tags = soup.find_all('a', class_="entry-title-link")
 
+print("\n== loop through a_tags ==\n")
 for a_tag in a_tags:
     print(a_tag.get_text())
 
 titles = list(map(lambda a_tag: a_tag.get_text(), a_tags))
 
+print("\n== titles map with a_tags ==\n")
 print(titles)
+
+print("\n== get article tags and drill deeper ==\n")
+
+blogs = soup.find_all('article')
+for blog in blogs:
+    title = blog.find('a', class_="entry-title-link").get_text()
+
+    blog_datetime_string = blog.find('time', class_="entry-time").get('datetime')
+    blog_datetime = datetime.datetime.fromisoformat(blog_datetime_string)
+    pretty_date = blog_datetime.strftime("%A, %B %m %Y")
+
+    print(f"{pretty_date} - {title}")
