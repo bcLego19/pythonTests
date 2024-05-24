1a299b70f1f6c64506e31159f8ee0319295e1752
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-24 13:43:01 -0700

    Update soup.py with beautiful soup

--- PythonProjects/scraper/soup.py
+++ PythonProjects/scraper/soup.py
@@ -1,6 +1,16 @@
 import requests, random
+from bs4 import BeautifulSoup
 
 url = "https://pixelford.com/blog"
 random_num = random.randint(1,999999)
 response = requests.get(url, headers={'user-agent': f'Hello{random_num}'})
-print(response.content)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+a_tags = soup.find_all('a', class_="entry-title-link")
+
+for a_tag in a_tags:
+    print(a_tag.get_text())
+
+titles = list(map(lambda a_tag: a_tag.get_text(), a_tags))
+
+print(titles)
