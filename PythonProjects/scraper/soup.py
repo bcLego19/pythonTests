8b36ccce80e961e193c132286893e4f548e3e9fb
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-24 13:33:11 -0700

    Update soup.py with header and new url

--- PythonProjects/scraper/soup.py
+++ PythonProjects/scraper/soup.py
@@ -1,5 +1,6 @@
-import requests
+import requests, random
 
-url = "https://linkedin.com"
-response = requests.get(url)
+url = "https://pixelford.com/blog"
+random_num = random.randint(1,999999)
+response = requests.get(url, headers={'user-agent': f'Hello{random_num}'})
 print(response.content)
