53bac406f1e8b940caeadac5354675c784bb35b0
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-26 12:39:51 -0700

    Add todo.py

---
+++ PythonProjects/todo/todo.py
@@ -0,0 +1,17 @@
+import sys
+
+# Read File
+
+# Add Todo
+
+# Remove Todo
+
+# Save File
+
+# Print List
+
+# Print Commands
+print("\n=========================================\n")
+print(f"To view ToDos:\n{sys.argv[0]}")
+print(f"\nTo add a ToDo:\n{sys.argv[0]} add \"Clean Room\"\n")
+print(f"To remove or complete ToDo:\n{sys.argv[0]} remove 2\n")
