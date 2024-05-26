cf3935f995da65d765625dec5d9ecf6d1f83237a
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-26 12:21:52 -0700

    Update edit.py for challenge problem

--- PythonLessons/edit.py
+++ PythonLessons/edit.py
@@ -6,5 +6,22 @@ file.close()
 lines = ["Hello\n", "My name is nick!"]
 
 # Write
-with open("modify.txt", "w"):
-    file.writelines(lines)
+file = open("modify.txt", "w")
+file.writelines(lines)
+file.close()
+
+file = open("numbers.txt", "r")
+lines = file.readlines()
+file.close()
+i = 0
+for i in range(len(lines)):
+    try:
+        lines[i] = str(float(lines[i]) * 2) + "\n"
+    except Exception as e:
+        pass
+
+print(lines)
+
+file = open("numbers.txt", "w")
+file.writelines(lines)
+file.close()
