70b89aaedf9df183616c2d37368b967a5635ed59
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-29 11:01:48 -0700

    Update ccwc to add main method

--- PythonChallenges/wc_tool/ccwc.py
+++ PythonChallenges/wc_tool/ccwc.py
@@ -4,8 +4,21 @@ Implements wc, which is a function for word, line, character, and byte count
 See man wc in your local terminal for more
 """
 
+import sys
+
 LEGAL_COMMANDS = ["-c"]
 
 def legal_commands(input, legal_input):
     return input in legal_input
 
+def main():
+    if (len(sys.argv) == 1):
+        print(f"ccwc {LEGAL_COMMANDS} \"test.txt\"")
+    elif (len(sys.argv) == 2 and legal_commands(sys.argv[1], LEGAL_COMMANDS)):
+        print("Too few arguments: Missing file")
+    elif (len(sys.argv) >= 3 and legal_commands(sys.argv[1], LEGAL_COMMANDS)):
+        print(sys.argv[1])
+    else:
+        print("Error in execution.")
+
+main()
