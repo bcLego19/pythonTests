2780b2a7422786f49953a3b9c0067cb091e86c96
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-29 13:41:29 -0700

    Update ccwc to accept multiple files

--- PythonChallenges/wc_tool/ccwc.py
+++ PythonChallenges/wc_tool/ccwc.py
@@ -44,7 +44,8 @@ def defaultAction(data, filename):
         byteCount = countBytes(filename)
         lineCount = countLines(data)
         wordCount = countWords(data)
-    print(f"{lineCount} {wordCount} {byteCount} {filename}")
+    print(f"    {lineCount} {wordCount} {byteCount} {filename}")
+    return [lineCount, wordCount, byteCount]
 
 def main():
     if (len(sys.argv) == 1):
@@ -66,28 +67,53 @@ def main():
                 charCount = countChars(data)
                 print(str(charCount))
     elif (len(sys.argv) >= 2 and not legal_commands(sys.argv[1], LEGAL_COMMANDS)):
-        filename = sys.argv[1]
-        data = read_data(filename)
-        if data:
-            defaultAction(data, filename)
-    elif (len(sys.argv) >= 3 and legal_commands(sys.argv[1], LEGAL_COMMANDS)):
-        command = sys.argv[1]
-        filename = sys.argv[2]
-        
-        data = read_data(filename)
-        if (command == "-c"):
-            byteCount = countBytes(sys.argv[2])
-            print(str(byteCount) + f" {sys.argv[2]}")
-        elif (command == "-l"):
-            lineCount = countLines(data)
-            print(str(lineCount) + f" {filename}")
-        elif (command == "-w"):
-            wordCount = countWords(data)
-            print(str(wordCount) + f" {filename}")
-        elif (command == "-m"):
-            charCount = countChars(data)
-            print(str(charCount) + f" {filename}")
+        totalBytes = 0
+        totalLines = 0
+        totalWords = 0
+        temp = []
+        for filename in sys.argv[1:]:
+            data = read_data(filename)
+            if data:
+                temp = defaultAction(data, filename)
+                totalLines += temp[0]
+                totalWords += temp[1]
+                totalBytes += temp[2]
+        print(f"    {totalLines}    {totalWords}    {totalBytes} total")
+
     else:
-        print("Error in execution.")
+        command = sys.argv[1]
+        totalBytes = 0
+        totalLines = 0
+        totalWords = 0
+        totalChars = 0
+        for filename in sys.argv[2:]:
+            data = read_data(filename)
+            if (command == "-c"):
+                byteCount = countBytes(sys.argv[2])
+                print(f"    {byteCount} {filename}")
+                totalBytes += byteCount
+            elif (command == "-l"):
+                lineCount = countLines(data)
+                print(f"    {lineCount} {filename}")
+                totalLines += lineCount
+            elif (command == "-w"):
+                wordCount = countWords(data)
+                print(f"    {wordCount} {filename}")
+                totalWords += wordCount
+            elif (command == "-m"):
+                charCount = countChars(data)
+                print(f"    {charCount}   {filename}")
+                totalChars += charCount
+        
+        if(totalBytes != 0):
+            print(f"    {totalBytes}    total")
+        elif(totalLines != 0):
+            print(f"    {totalLines}    total")
+        elif(totalWords != 0):
+            print(f"    {totalWords}    total")
+        elif(totalChars != 0):
+            print(f"    {totalChars}    total")
+        else:
+            print("Error counting totals :\\")
 
 main()
