22d4e92609f419a7ed9828fd3c8be26673795874
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-29 10:53:17 -0700

    Update ccwc with legal commands check

--- PythonChallenges/wc_tool/ccwc.py
+++ PythonChallenges/wc_tool/ccwc.py
@@ -3,3 +3,9 @@ Code your own word count program:
 Implements wc, which is a function for word, line, character, and byte count
 See man wc in your local terminal for more
 """
+
+LEGAL_COMMANDS = ["-c"]
+
+def legal_commands(input, legal_input):
+    return input in legal_input
+
