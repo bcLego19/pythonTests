f36c2dcea414a2a5d4bf828bde67f2f80f2533b4
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-05-29 12:11:58 -0700

    Update ccwc to count lines

--- PythonChallenges/wc_tool/ccwc.py
+++ PythonChallenges/wc_tool/ccwc.py
@@ -6,10 +6,18 @@ See man wc in your local terminal for more
 
 import sys
 
-LEGAL_COMMANDS = ["-c"]
+LEGAL_COMMANDS = ["-c", "-l"]
 
 def legal_commands(input, legal_input):
     return input in legal_input
+
+def read_file(filename):
+  try:
+    with open(filename, "rb") as file:
+      return file.read().decode("utf-8")  # Decode for character processing
+  except Exception as e:
+    print(f"Error opening file: {e}")
+    return None
 
 def countBytes(FILENAME):
     try:
@@ -18,6 +26,9 @@ def countBytes(FILENAME):
     except Exception as e:
         print(e)
 
+def countLines(data):
+  return len(data.splitlines())
+
 def main():
     if (len(sys.argv) == 1):
         print(f"ccwc {LEGAL_COMMANDS} \"test.txt\"")
@@ -25,11 +36,15 @@ def main():
         print("Too few arguments: Missing file")
     elif (len(sys.argv) >= 3 and legal_commands(sys.argv[1], LEGAL_COMMANDS)):
         command = sys.argv[1]
-        #print(command)
+        filename = sys.argv[2]
         
+        data = read_file(filename)
         if (command == "-c"):
             byteCount = countBytes(sys.argv[2])
             print(str(byteCount) + f" {sys.argv[2]}")
+        elif (command == "-l"):
+            lineCount = countLines(data)
+            print(str(lineCount) + f" {filename}")
     else:
         print("Error in execution.")
 
