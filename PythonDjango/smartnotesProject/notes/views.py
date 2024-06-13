c1366759a51e5be246cab19cf017e688ab520dc4
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-06-13 15:44:38 -0700

    Update notes view to add notes list view class

--- PythonDjango/smartnotesProject/notes/views.py
+++ PythonDjango/smartnotesProject/notes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Notes
+from django.views.generic import ListView
 
 # Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
