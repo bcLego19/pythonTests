b226b304305a7223f2039dbd5118eaa3957c2b31
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-06-13 15:45:46 -0700

    Update notes view to remove unnecessary endpoint

--- PythonDjango/smartnotesProject/notes/views.py
+++ PythonDjango/smartnotesProject/notes/views.py
@@ -8,10 +8,6 @@ class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
-
 def detail(request, pk):
     try:
         note = Notes.objects.get(pk=pk)
