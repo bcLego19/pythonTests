4a6c40936a83d82aec4fc02d3652ab3f4711ac9c
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-08-07 11:05:13 -0700

    Update todo project with view and index

--- PythonDjango/todo-project/todo/views.py
+++ PythonDjango/todo-project/todo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
+def index(request):
+    todo_list = Todo.objects.all()
+    return render(request, 'todo/index.html', {'todo_list': todo_list})
