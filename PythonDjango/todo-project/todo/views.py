8360104ffbe1affa6de1def592495ca08bd58532
bcLego19 <48166760+bcLego19@users.noreply.github.com>
2024-08-14 14:35:54 -0700

    Update todo project to use forms

--- PythonDjango/todo-project/todo/views.py
+++ PythonDjango/todo-project/todo/views.py
@@ -5,8 +5,20 @@ from .forms import TodoForm
 # Create your views here.
 def index(request):
     todo_list = Todo.objects.all()
+    # Use an empty form instance for displaying the form
     form = TodoForm()
-    return render(request, 'todo/index.html', {'form': form})
+    if request.method == 'POST':
+        # Create a form instance with the submitted data
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            # Access completed checkboxes using form.cleaned_data['completed']
+            completed_ids = request.POST.getlist('completed')  # Get a list of selected checkbox values
+            for todo_id in completed_ids:
+                todo = Todo.objects.get(pk=todo_id)
+                todo.completed = not todo.completed  # Toggle completed status
+                todo.save()
+            return redirect('/')
+    return render(request, 'todo/index.html', {'todo_list': todo_list, 'form': form})
 
 def add_todo(request):
     if request.method == 'POST':
